890e72d1f4fb83fcc028c08c3a72a411329480a2
Sergi SC <sersac78@hotmail.com>
2019-07-18 12:29:42 +0200

    Segona versió

--- js/pcliga.js
+++ js/pcliga.js
@@ -1,15 +1,18 @@
-/* global $, document , console, alert*/
+/* global $, document , alert*/
 
 $(document).ready(function () {
 
     "use strict";
 
+    var clau;
+    var fitxa;
+    
+    // CLÀUSULA
+
 
     $("#boto").on("click", function () {
 
-        var clau = $("#clau").val();
-
-
+        clau = $("#clau").val();
 
         var calcul = parseInt(clau * 0.1499) + parseInt(clau);
 
@@ -18,12 +21,14 @@ $(document).ready(function () {
         buidar();
 
     });
+    
 
+    // FITXA
 
     $("#boto_f").on("click", function () {
 
-        var fitxa = $("#fitxa").val();
-
+        fitxa = $("#fitxa").val();
+        var res;
 
         var opcio = parseInt(prompt("Clàusula:\n\t1. <30.000.000 \n\t2. Entre 30.000.001 i 50.000.000 \n\t3. Entre 50.000.001 i 70.000.000 \n\t4. Entre 70.000.001 i 90.000.000 \n\t5. >90.000.000"));
 
@@ -31,27 +36,27 @@ $(document).ready(function () {
 
             case 1:
 
-                var res = parseInt(fitxa * 0.18035) + parseInt(fitxa);
+                res = parseInt(fitxa * 0.18035) + parseInt(fitxa);
                 break;
 
             case 2:
 
-                var res = parseInt(fitxa * 0.230104) + parseInt(fitxa);
+                res = parseInt(fitxa * 0.230104) + parseInt(fitxa);
                 break;
 
             case 3:
 
-                var res = parseInt(fitxa * 0.280044) + parseInt(fitxa);
+                res = parseInt(fitxa * 0.280044) + parseInt(fitxa);
                 break;
 
             case 4:
 
-                var res = parseInt(fitxa * 0.330023) + parseInt(fitxa);
+                res = parseInt(fitxa * 0.330023) + parseInt(fitxa);
                 break;
 
             case 5:
 
-                var res = parseInt(fitxa * 0.38) + parseInt(fitxa);
+                res = parseInt(fitxa * 0.38) + parseInt(fitxa);
                 break;
 
             default:
@@ -61,7 +66,7 @@ $(document).ready(function () {
 
         }
         var resultat2 = $("<p></p>").text(" La fitxa és: " + res);
-        $(".clausula").append(resultat2);
+        $(".fitxa").append(resultat2);
         buidar();
 
 
@@ -69,14 +74,9 @@ $(document).ready(function () {
 
 
     function buidar() {
-        var clau = $("#clau").val("");
-        var fitxa = $("#fitxa").val("");
+        clau = $("#clau").val("");
+        fitxa = $("#fitxa").val("");
     }
-
-
-
-
-
 
 
 });
